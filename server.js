0431f22300f10f7b5505ec1a8deb40dc1ddabf12
tim901231 <b09901020@ntu.edu.tw>
2023-10-07 00:52:32 +0800

    fix

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 // serve up production assets
-app.use(express.static('client/build'));
+app.use(express.static('build'));
 // let the react app to handle any unknown routes 
 // serve up the index.html if express does'nt recognize the route
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 // if not in production use the port 5000
 const PORT = process.env.PORT || 5000;
