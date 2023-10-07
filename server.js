9e03472a28862ee338941b9bb5938c4cc94c61bd
tim901231 <b09901020@ntu.edu.tw>
2023-10-07 10:47:19 +0800

    try_video

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const path = require('path');
+const multer = require('multer');
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 100 * 1024 * 1024 }
+});
+
+
 
 const app = express();
 // serve up production assets
 app.use(express.static('build'));
+
 // let the react app to handle any unknown routes 
 // serve up the index.html if express does'nt recognize the route
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
+
+app.post('/upload', upload.single('video'), async (req, res) => {
+    const videoData = req.file.buffer;
+
+    res.sendStatus(200);
+})
+
 // if not in production use the port 5000
 const PORT = process.env.PORT || 5000;
 console.log('server started on port:',PORT);
